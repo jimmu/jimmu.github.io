7b917033b2d669c67d58a02cedb5d45ef5612180
James <james.hunter@rbs.com>
2015-04-24 15:27:02 +0100

    more circles

--- visualwhimsy.js
+++ visualwhimsy.js
@@ -1,18 +1,33 @@
-var circleData = [];
+var width=600;
+var height=500;
+var numCircles=20;
 
-for (var i=0; i<20; i++){
-  var thisCircle = {"cx": i*10, "cy": i*20, "r": i*4, "colour": "black"};
-  circleData.push(thisCircle);
+
+function createCircleData(){
+	var maxRadius = 50;
+        var maxX = width-maxRadius;
+	var maxY = height-maxRadius;
+
+	var circleData = [];
+	for (var i=1; i<=numCircles; i++){
+	  var thisCircle = {"cx": i*maxX/numCircles
+			, "cy": i*maxY/numCircles
+			, "r": i*maxRadius/numCircles
+			, "colour": "black"
+			};
+	  circleData.push(thisCircle);
+	}
+	return circleData;
 }
 
 var svg=d3.select("body").append("svg")
-	.attr("width", 500)
-	.attr("height", 500);
+	.attr("width", width)
+	.attr("height", height);
 
 var circleGroup = svg.append("g");
 
 var circles = circleGroup.selectAll("circle")
-		.data(circleData)
+		.data(createCircleData())
 		.enter()
 		.append("circle");
 
