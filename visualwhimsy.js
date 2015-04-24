ae0cf2cf2f3bf7f5b5e5a7a8e9368b1f5625e33d
James <james.hunter@rbs.com>
2015-04-24 15:06:58 +0100

    hello svg

--- visualwhimsy.js
+++ visualwhimsy.js
@@ -1,2 +1,22 @@
-var div=d3.select("body").append("div");
-div.append("span").text("four oh four");
+var circleData = [];
+
+for (var i=0; i<10; i++){
+  var thisCircle = {"cx": i*10, "cy": i*20, "r": i*4, "colour": "black"};
+  circleData.push(thisCircle);
+}
+
+var svg=d3.select("body").append("svg")
+	.attr("width", 500)
+	.attr("height", 500);
+
+var circleGroup = svg.append("g");
+
+var circles = circleGroup.selectAll("circle")
+		.data(circleData)
+		.enter()
+		.append("circle");
+
+circles.attr("cx", function(d){return d.cx})
+	.attr("cy", function(d){return d.cy})
+	.attr("r", function(d){return d.r})
+	.attr("fill", function(d){return d.colour});
