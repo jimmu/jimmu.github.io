4f86c7b2060edc6a78e3ceca0c2144c09a6e985a
James Hunter <james@MacBookAir.communityfibre.co.uk>
2024-11-28 17:22:19 +0000

    Touch controls

--- thrust/controls.js
+++ thrust/controls.js
@@ -8,16 +8,17 @@ const downKey="l".toUpperCase().charCodeAt(0)
 
 export function newControls(){
     return {
-        leftPressed, rightPressed, upPressed, downPressed, directions
+        directions
     }
 
     // Return a single direction object containing left/right/up/down as values from 0 to 1?
     function directions(){
+        let touchDirections = checkTouches()
         return {
-            left: leftPressed()? 1 : 0,
-            right: rightPressed()? 1 : 0,
-            up: upPressed()? 1 : 0,
-            down: downPressed()? 1 : 0
+            left: touchDirections.left + leftPressed()? 1 : 0,
+            right: touchDirections.right + rightPressed()? 1 : 0,
+            up: touchDirections.up + upPressed()? 1 : 0,
+            down: touchDirections.down + downPressed()? 1 : 0
         }
     }
 
@@ -39,6 +40,26 @@ export function newControls(){
     }
 
     function checkTouches(){
+        let directions = {left: 0, right: 0, up: 0, down: 0}
+        for (let touch of p5.touches){
+            // Touch.x/y are relative to the top left of the canvas.
+            // Touch.winX/Y are relative to the browser
+            // Either way, let's divide the space into left quarter and right quarter
+            // and the middle half into top and bottom
+            if (touch.winX > p5.windowWidth * 0.75){
+                directions.right = 1
+            }
+            else if (touch.winX < p5.windowWidth * 0.25){
+                directions.left = 1
+            }
+            else if (touch.winY < p5.windowHeight/2){
+                directions.up = 1
+            }
+            else if (touch.winY > p5.windowHeight/2){
+                directions.down = 1
+            }
+        }
+        return directions
     }
 
 }
